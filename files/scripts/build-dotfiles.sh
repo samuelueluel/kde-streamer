d85dfd48e21aceb5f01b36283bc215dfa5be4fa7
Samuel <samuel@example.com>
2026-05-21 10:36:08 -0600

    update

--- files/scripts/build-dotfiles.sh
+++ files/scripts/build-dotfiles.sh
@@ -2,10 +2,24 @@
 
 set -ouex pipefail
 
-echo "Baking dotfiles snapshot from public repo..."
+echo "Fetching minimal dotfiles (zsh + Brewfile) from public repo..."
 
 DOTFILES_OWNER="${DOTFILES_OWNER:-samuelueluel}"
-git clone --depth=1 "https://github.com/${DOTFILES_OWNER}/dotfiles.git" /usr/share/kde-streamer/dotfiles
-rm -rf /usr/share/kde-streamer/dotfiles/.git
+WORK_DIR=$(mktemp -d)
+
+git clone --depth=1 --filter=blob:none --no-checkout \
+    "https://github.com/${DOTFILES_OWNER}/dotfiles.git" "$WORK_DIR"
+
+cd "$WORK_DIR"
+git sparse-checkout init --cone
+git sparse-checkout set dot_zshrc dot_zshenv dot_Brewfile
+git checkout
+
+mkdir -p /usr/share/kde-streamer
+cp -v "$WORK_DIR/dot_zshrc"   /usr/share/kde-streamer/
+cp -v "$WORK_DIR/dot_zshenv"  /usr/share/kde-streamer/
+cp -v "$WORK_DIR/dot_Brewfile" /usr/share/kde-streamer/
+
+rm -rf "$WORK_DIR"
 
 chmod +x /usr/bin/kjust
